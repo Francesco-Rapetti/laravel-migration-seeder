09b1b227d3c9cea67f4a17920bd7a798546ae805
Francesco-Rapetti <rapettif80@yahoo.it>
2024-01-26 12:11:51 +0100

    Seeder trains table

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $trains = [
+            [
+                'company' => 'Trenitalia',
+                'departure_station' => 'Milano',
+                'arrival_station' => 'Roma',
+                'departure_time' => '2023-01-01 10:00:00',
+                'arrival_time' => '2023-01-01 12:00:00',
+                'train_code' => 'TRE123',
+                'number_of_carriages' => 20
+            ],
+            [
+                'company' => 'Trenitalia',
+                'departure_station' => 'Milano',
+                'arrival_station' => 'Napoli',
+                'departure_time' => '2023-01-01 10:00:00',
+                'arrival_time' => '2023-01-01 12:00:00',
+                'train_code' => 'TRE124',
+                'number_of_carriages' => 20
+            ]
+        ];
+
+        foreach ($trains as $train) {
+            $newTrain = new Train();
+            $newTrain->company = $train['company'];
+            $newTrain->departure_station = $train['departure_station'];
+            $newTrain->arrival_station = $train['arrival_station'];
+            $newTrain->departure_time = $train['departure_time'];
+            $newTrain->arrival_time = $train['arrival_time'];
+            $newTrain->train_code = $train['train_code'];
+            $newTrain->number_of_carriages = $train['number_of_carriages'];
+            $newTrain->save();
+        }
+    }
+}
