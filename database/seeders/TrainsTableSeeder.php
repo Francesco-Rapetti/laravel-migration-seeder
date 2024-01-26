f0bdf407b24359f9e244b55bbaf5eea838e902ba
Francesco-Rapetti <rapettif80@yahoo.it>
2024-01-26 12:35:31 +0100

    Add 5 new fake trains

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,13 +5,14 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         $trains = [
             [
@@ -31,7 +32,7 @@ class TrainsTableSeeder extends Seeder
                 'arrival_time' => '2023-01-01 12:00:00',
                 'train_code' => 'TRE124',
                 'number_of_carriages' => 20
-            ]
+            ],
         ];
 
         foreach ($trains as $train) {
@@ -43,6 +44,18 @@ class TrainsTableSeeder extends Seeder
             $newTrain->arrival_time = $train['arrival_time'];
             $newTrain->train_code = $train['train_code'];
             $newTrain->number_of_carriages = $train['number_of_carriages'];
+            $newTrain->save();
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            $newTrain = new Train();
+            $newTrain->company = $faker->company();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->dateTimeBetween('-1 month', '+1 month');
+            $newTrain->arrival_time = $faker->dateTimeBetween('-1 month', '+1 month');
+            $newTrain->train_code = $faker->bothify('???-????');
+            $newTrain->number_of_carriages = $faker->numberBetween(10, 30);
             $newTrain->save();
         }
     }
